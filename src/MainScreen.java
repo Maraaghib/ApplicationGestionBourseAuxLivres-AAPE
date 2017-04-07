172cd12bf908cf69301ae9d072580e49d1393c32
Pierre Lorson <plorson@emi.u-bordeaux1.fr>
2017-04-07 12:05:29 +0200

    Import/Export fonctionnel, tableau adhérents fonctionnel.

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -1,5 +1,7 @@
 import javax.swing.JFrame;
+
 import java.awt.GridLayout;
+
 import javax.swing.JButton;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
@@ -7,39 +9,55 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 import java.awt.event.ActionListener;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
 import java.awt.event.ActionEvent;
+
 import javax.swing.JFileChooser;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 public class MainScreen extends JFrame {
 	/**
 	 * Test
 	 */
+	File source = new File("/autofs/netapp/account/cremi/plorson/projtrans/GeBouL/database/GeBouL.db");
 	private static final long serialVersionUID = 1L;
 	private JPanel panel;
 	public MainScreen() {
 		getContentPane().setLayout(new GridLayout(0, 2, 0, 0));
 		
-		JButton btnNewButton = new JButton("Gestion des adhérents");
+		JButton btnNewButton = new JButton("Gestion des adh�rents");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println ("Test Gestion");
 				AdherentsGUI adh = new AdherentsGUI();
-		        adh.setSize(500, 500);
-		        adh.setVisible(true);
+		        adh.createAndShowGUI();
 				
 			}
 		});
 		getContentPane().add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Gestion du stock");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println ("Test Gestion");
+				StockGUI stock = new StockGUI();
+		        stock.createAndShowGUI();
+				
+			}
+		});
 		getContentPane().add(btnNewButton_1);
+	
 		
 		JButton btnNewButton_2 = new JButton("Importer");
 		getContentPane().add(btnNewButton_2);
@@ -47,11 +65,11 @@ public class MainScreen extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println ("Test importer");
 				JFileChooser fc = new JFileChooser();
-				
-				BufferedReader reader = null;
+				File dest = null;
 				try{
 				if(fc.showOpenDialog(btnNewButton_2) == JFileChooser.APPROVE_OPTION)
-					reader = new BufferedReader(new FileReader(fc.getSelectedFile()));
+					dest = fc.getSelectedFile();
+					Files.copy(dest.toPath(), source.toPath(),StandardCopyOption.REPLACE_EXISTING);
 				} catch (IOException e1) {
 					System.out.println ("Test importer");
 					e1.printStackTrace();
@@ -67,13 +85,13 @@ public class MainScreen extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println ("Test Exporter");
 				JFileChooser fc = new JFileChooser();
-				
-				BufferedReader reader = null;
+				File dest = null;
 				try{
-				if(fc.showOpenDialog(btnNewButton_3) == JFileChooser.APPROVE_OPTION)
-					reader = new BufferedReader(new FileReader(fc.getSelectedFile()));
+				if(fc.showSaveDialog(btnNewButton_3) == JFileChooser.APPROVE_OPTION){
+					dest = fc.getSelectedFile();
+					Files.copy(source.toPath(), dest.toPath());
+				}
 				} catch (IOException e1) {
-					System.out.println ("Test exporter");
 					e1.printStackTrace();
 				}
 			}
@@ -91,6 +109,20 @@ public class MainScreen extends JFrame {
 		
 		JMenuItem mntmGestionDuStock = new JMenuItem("Gestion du stock");
 		mnFichier.add(mntmGestionDuStock);
+		
+		JMenuItem mntmImprimer = new JMenuItem("Imprimer");
+		mnFichier.add(mntmImprimer);
+		mntmImprimer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			PrinterJob pj = PrinterJob.getPrinterJob();
+			    if (pj.printDialog()) {
+			        try {pj.print();}
+			        catch (PrinterException exc) {
+			            System.out.println(exc);
+		         }
+		     }
+			}
+		});
 	}
 
 	public static void main(String[] args) {
