e291401904def147c84402c1b124e52958c09fc0
Serigne Amsatou SEYE <serigne-amsatou.seye@etu.u-bordeaux.fr>
2017-04-15 23:22:37 +0200

    By Adrian: Add files via upload

---
+++ src/Adherant.java
@@ -0,0 +1,94 @@
+import java.util.ArrayList;
+public class Adherant {
+	private long Id;
+	private long Id_adherent;
+	private String Prenom;
+	private String Nom;
+	private String Num_telephone;
+	private String adresse;
+	private String adresse_mail ;
+	private ArrayList<Commande> listCommande;
+	
+	/**Constructors**/
+	public Adherant(){
+		
+	}
+	public Adherant(long Id,long Id_adherent,String Prenom,String Nom,String Num_telephone,String adresse,String adresse_mail ,ArrayList<Commande> listCommande ){
+		this.Id=Id;
+		this.Id_adherent=Id_adherent;
+		this.Prenom=Prenom;
+		this.Nom=Nom;
+		this.Num_telephone=Num_telephone;
+		this.adresse_mail=adresse_mail;
+		this.listCommande=listCommande;
+		this.adresse=adresse;
+	}
+	/**Controllers**/
+				/**Get**/
+	public long getId(){
+		return Id;
+	}
+	public void setId(long Id){
+		this.Id=Id;
+	}
+	public long getIdadherent(){
+		return this.Id_adherent;
+	}
+	public String getPrenom(){
+		return this.Prenom;
+	}
+	public String getNom(){
+		return this.Nom;
+	}
+	public String getNum_telephone(){
+		return this.Num_telephone;
+	}
+	public String getadresse(){
+		return this.adresse;
+	}
+	public String getadresse_mail(){
+		return this.adresse_mail;
+	}
+	public ArrayList<Commande> getListCommande(){
+		return this.listCommande;
+	}
+	
+				/**Set**/
+	public void setPrenom(String Prenom){
+		this.Prenom=Prenom;
+	}
+	public void setNom(String Nom){
+		this.Nom=Nom;
+	}
+	public void setNum_telephone(String Num_telephone){
+		this.Num_telephone=Num_telephone;
+	}
+	public void setadresse(String adresse){
+		this.adresse=adresse;
+	}
+	public void setadresse_mail(String adresse_mail){
+		this.adresse_mail=adresse_mail;
+	}
+	public void setListCommande(ArrayList<Commande> listCommande){
+		this.listCommande=listCommande;
+	}
+	
+	
+	public void addCommande(Commande com){
+		this.listCommande.add(com);
+	}
+	public Commande getCommande(int indice){
+		return this.listCommande.get(indice);
+	}
+	public String toString(){
+		double somme =0;
+		StringBuffer s = new StringBuffer("Récapitulatif des commandes de l'adherent");
+		for(int i=0;i<listCommande.size();i++){
+			s.append("Commande n°"+i);
+			s.append(listCommande.get(i).toString());
+			somme+=listCommande.get(i).facture();
+		}
+		s.append("La facture totale est de"+somme+" euros " );
+		return new String(s);
+	}
+}
---
+++ src/Commande.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+public class Commande {
+	private long Id;
+	private int classe;
+	private boolean Veux_une_Calculatrice;
+	private ArrayList<Livre> listeLivre=new ArrayList<Livre>();
+	/**Constructor*/
+	public Commande(){
+		
+	}
+	public Commande(long Id,int classe,boolean Veux_une_calculatrice,ArrayList<Livre> liste){
+		this.Id=Id;
+		this.classe=classe;
+		this.Veux_une_Calculatrice=Veux_une_calculatrice;
+		this.listeLivre=liste;
+	}
+	
+	/**Controllers**/
+	public long getId(){
+		return Id;
+	}
+	public int getClasse(){
+		return this.classe;
+	}
+	public boolean getVeux_une_calculatrice(){
+		return this.Veux_une_Calculatrice;
+	}
+	public ArrayList<Livre> getListLivre(){
+		return this.listeLivre;
+	}
+	public void setId(long Id){
+		this.Id=Id;
+	}
+	public void setClasse(int classe){
+		this.classe=classe;
+	}
+	public void setVeux_une_calculatrice(boolean calculatrice){
+		this.Veux_une_Calculatrice=calculatrice;
+	}
+	public void setListLivre (ArrayList<Livre> listLivre){
+		this.listeLivre=listLivre;
+	}
+	public void addLivre (Livre book){
+		this.listeLivre.add(book);
+	}
+	
+	
+	public Commande clone(){
+		return new Commande(this.Id,this.classe,this.Veux_une_Calculatrice,this.listeLivre);
+	}
+	public Livre getLivre(int indice){
+		return this.listeLivre.get(indice);
+	}
+
+	public double facture(){
+		double somme=0;
+		for(int i=0;i<listeLivre.size();i++){
+			somme+=listeLivre.get(i).getPrix();
+		}
+		return somme;
+	}
+	
+
+	public String toString(){
+		StringBuffer s = new StringBuffer("Ceci est une commande pour un élève de classe de "+this.getClasse()+"et il désire la liste des livres suivants:");
+		s.append("/n");
+		for(int i=0;i<this.listeLivre.size();i++){
+			s.append(this.listeLivre.get(i).toString());
+			s.append("/n");
+		}
+		if(getVeux_une_calculatrice()){
+		    s.append(" et il possède une calculatrice.");
+		}
+		else {
+			s.append("il ne possède pas de calculatrice , et il en désire une.");
+		}
+		s.append("Le pix total de cette commande est de ");
+		s.append(this.facture());
+		return new String(s);
+		
+	}
+	
+}
